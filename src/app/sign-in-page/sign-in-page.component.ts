2b3c1d9a616d96df93190b9f91a031b8c0dda7da
Edwin Ekpenyong <moogiemode@gmail.com>
2021-07-31 23:04:28 -0700

    added icon, added dynamic property for password field

--- src/app/sign-in-page/sign-in-page.component.ts
+++ src/app/sign-in-page/sign-in-page.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in-page.component.scss']
 })
 export class SignInPageComponent implements OnInit {
+  passwordShown = false;
 
-  constructor() { }
+  passwordField = this.passwordShown ? 'text' : 'password';
+  showHideImg = this.passwordShown
+    ? '../../assets/icon--eye-open.svg'
+    : '../../assets/icon--eye-closed.svg';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
